0ebd34785d5b509fef5d189419a9c348adbf4541
lars <60745347+lars-ruijs@users.noreply.github.com>
2021-01-05 14:38:28 +0100

    Add basic FTM styling

--- datavis-l/src/App.vue
+++ datavis-l/src/App.vue
@@ -7,12 +7,84 @@
 </template>
 
 <style>
+/* Import external fonts:
+   Page titles: 'Open Sans Condensed', sans-serif
+   Page body: 'PT Serif', serif;
+   Graph text: 'PT Sans', sans-serif;
+*/
+
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:wght@700&family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&display=swap');
+
+:root {
+  --body-color: #000000;
+  --link-color: #39b2b1;
+  --ftm-red: #dd3230;
+  --ftm-grey: #f6f6f6;
+  --ftm-blue: #d1e8ea;
+  --ftm-title: 'Open Sans Condensed', sans-serif;
+  --ftm-body: 'PT Serif', serif;
+  --ftm-graph: 'PT Sans', sans-serif;  
+}
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+h1, h2, h3 {
+  font-family: var(--ftm-title);
+  color: var(--body-color);
+  line-height: 1.2em;
+}
+
+h1 {
+  font-size: 3em;
+  margin: 1em 0;
+}
+
+.home h1:first-child {
+  margin-top: 3em;
+}
+
+p {
+  font-family: var(--ftm-body);
+  color: var(--body-color);
+  margin: 1em 0;
+}
+
+p.summary {
+  font-family: var(--ftm-body);
+  font-weight: bold;
+}
+
+.home {
+  margin: 0 auto;
+  width: 50%;
+}
+
+.top-bar {
+  height: 3.8em;
+  width: 100%;
+  background-color: #292f32;
+  position: fixed;
+  top: 0;
+}
+
+.top-bar img.eye {
+  height: 3.8em;
+}
+.top-bar img.logo {
+  height: 1.5em;
+  margin: 0 0 1em 1em;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+/* font-family: 'Hanalei Fill', cursive;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #2c3e50; */
 }
 
 #nav {
--- datavis-l/src/views/Home.vue
+++ datavis-l/src/views/Home.vue
@@ -1,7 +1,17 @@
 <template>
+ <div class="top-bar">
+   <img class="eye" src="@/assets/ftm-eye.png" alt="oog FTM">
+   <img class="logo" src="@/assets/wordmark.png" alt="Follow the Money">
+ </div>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <h1>Dashboard online advertenties politieke partijen</h1>
+      <p class="summary">
+        De campagnes voor de Tweede Kamerverkiezingen zijn inmiddels in volle gang, ook via advertenties op Facebook, Instagram, Google en YouTube. Follow the Money brengt de geldstromen van online advertenties in kaart. Hieruit blijkt dat bijna alle partijen de afgelopen tijd voor tienduizenden euro aan advertenties kochten.
+      </p>
+      <p>
+        Op 17 maart 2021 zijn de Tweede Kamerverkiezingen. De campagnes daarvoor zijn inmiddels in volle gang, ook via advertenties op Facebook, Instagram, Google en YouTube. Hieronder zie je hoeveel geld de Nederlandse politieke partijen sinds september bij deze platforms hebben uitgeven.
+      </p>
+      <HelloWorld msg="Welcome to Jouw Vue.js App"/>
   </div>
 </template>
 
